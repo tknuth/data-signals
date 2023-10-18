1942c48fa04808bc5d1b6dbd813bf87462be8299
Tobias Knuth <5556920+tknuth@users.noreply.github.com>
2023-10-18 15:07:15 +0200

    Update aggregations.py

--- src/panda_detective/aggregations.py
+++ src/panda_detective/aggregations.py
@@ -12,6 +12,6 @@ def count_notna(x: pd.DataFrame | pd.Series):
 def count_na(x: pd.DataFrame | pd.Series):
     return x.isna().sum()
 
-
+# TODO: format as fraction, i.e. [a, a+b]
 def count_notna_na(x: pd.DataFrame | pd.Series):
     return [x.notna().sum(), x.isna().sum()]
